a3f7930bad892d47648874931e11740734402108
SamsAgency <samstore637@gmail.com>
2021-12-16 16:32:30 +0300

    worked on th lazy routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import "./App.css";
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/home";
-import About from "./components/About/about";
-import Service from "./components/Services/service";
-import Contact from "./components/Contact/contact";
-import Auth from "./components/Auth/auth";
-import Dashboard from "./components/User/Dashboard/dashboard";
-import NotFound from "./components/404/404";
-import Quotation from "./components/User/quotation/quotation";
-import Reset from "./components/User/Reset/reset";
-import Shipment from "./components/shipment/shipment";
-import Logistics from "./components/Home/Service/ServicePages/Logistics/Logistics";
-import Storage from "./components/Home/Service/ServicePages/Storage/Storage";
-import Ocean from "./components/Home/Service/ServicePages/Ocean/Ocean";
-import Brokerage from "./components/Home/Service/ServicePages/Brokerage/Brokerage";
-import Air from "./components/Home/Service/ServicePages/Air/Air";
-import Linkages from "./components/Home/Service/ServicePages/Linkages/Linkages";
-import Advisory from "./components/Home/Service/ServicePages/Advisory/Advisory";
-import Product from "./components/Product/product";
-import ProductQuotation from "./components/Forms/ProductQuotation/ProductQuotation";
-import ServiceType from "./components/Forms/ServiceQuotation/ServiceType/ServiceType";
-import ServiceQuotation from "./components/Forms/ServiceQuotation/ServiceQuotation";
-import LogisticsQuotation from "./components/Forms/LogisticsQuotation/LogisticsQuotation";
-import ShipmentTable from "./components/shipment/shipmentTable/ShipmentDataTable/ShipmentTable";
-import StorageTable from "./components/shipment/shipmentTable/StorageTable/StorageTable";
-import Refining from "./components/Home/Service/ServicePages/Refining/Refining";
+import {
+	LazyHome,
+	LazyAbout,
+	LazyServices,
+	LazyProduct,
+	LazyContact,
+	LazyAuth,
+	LazyDashboard,
+	LazyShipment,
+	LazyQuotation,
+	LazyReset,
+	LazyLogistics,
+	LazyStorage,
+	LazyBrokerage,
+	LazyAir,
+	LazyLinkages,
+	LazyAdvisory,
+	LazyServiceType,
+	LazyOcean,
+	LazyRefining,
+	LazyProductQuotation,
+	LazyServiceQuotation,
+	LazyLogisticsQuotation,
+	LazyShipmentTable,
+	LazyStorageTable,
+	LazyNotFound
 
+} from "./components/utilities/lazyroutes"
 function App() {
   return (
 	<div className="App">
@@ -34,31 +36,31 @@ function App() {
 		{/* <Navbar/> */}'
 		<React.Suspense fallback="Rhinojohn loading...">
 			<Switch>
-				<Route path="/" exact component={Home} />
-				<Route path="/about" component={About} />
-				<Route path="/products" component={Product} />
-				<Route path="/services" component={Service} />
-				<Route path="/contact" component={Contact} />
-				<Route path="/auth" component={Auth} />
-				<Route path="/dashboard" component={Dashboard} />
-				<Route path="/shipment" component={Shipment} />
-				<Route path="/quotation" component={Quotation} />
-				<Route path="/reset" component={Reset} />
-				<Route path="/services-logistics" component={Logistics} />
-				<Route path="/services-storage" component={Storage} />
-				<Route path="/services-customs" component={Brokerage} />
-				<Route path="/services-air" component={Air} />
-				<Route path="/services-land" component={Linkages} />
-				<Route path="/services-trade" component={Advisory} />
-				<Route path="/service-type" component={ServiceType} />
-				<Route path="/services-ocean" component={Ocean} />
-				<Route path="/services-refinery" component={Refining} />
-				<Route path="/product-quotation" component={ProductQuotation} />
-				<Route path="/storage-quotation" component={ServiceQuotation} />
-				<Route path="/logistics-quotation" component={LogisticsQuotation} />
-				<Route path="/track-shipment" component={ShipmentTable}/>
-				<Route path="/track-storage" component={StorageTable}/>
-				<Route component={NotFound} />
+				<Route path="/" exact component={LazyHome} />
+				<Route path="/about" component={LazyAbout} />
+				<Route path="/products" component={LazyProduct} />
+				<Route path="/services" component={LazyServices} />
+				<Route path="/contact" component={LazyContact} />
+				<Route path="/auth" component={LazyAuth} />
+				<Route path="/dashboard" component={LazyDashboard} />
+				<Route path="/shipment" component={LazyShipment} />
+				<Route path="/quotation" component={LazyQuotation} />
+				<Route path="/reset" component={LazyReset} />
+				<Route path="/services-logistics" component={LazyLogistics} />
+				<Route path="/services-storage" component={LazyStorage} />
+				<Route path="/services-customs" component={LazyBrokerage} />
+				<Route path="/services-air" component={LazyAir} />
+				<Route path="/services-land" component={LazyLinkages} />
+				<Route path="/services-trade" component={LazyAdvisory} />
+				<Route path="/service-type" component={LazyServiceType} />
+				<Route path="/services-ocean" component={LazyOcean} />
+				<Route path="/services-refinery" component={LazyRefining} />
+				<Route path="/product-quotation" component={LazyProductQuotation} />
+				<Route path="/storage-quotation" component={LazyServiceQuotation} />
+				<Route path="/logistics-quotation" component={LazyLogisticsQuotation} />
+				<Route path="/track-shipment" component={LazyShipmentTable}/>
+				<Route path="/track-storage" component={LazyStorageTable}/>
+				<Route component={LazyNotFound} />
 			</Switch>
 		</React.Suspense>
 	  </Router>
